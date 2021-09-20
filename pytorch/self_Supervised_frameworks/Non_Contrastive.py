18bb31de0d6d8a95d7ae175ee4d2e3118c2e72ce
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2021-09-20 11:38:21 +0800

    Create Non_Contrastive.py

---
+++ pytorch/self_Supervised_frameworks/Non_Contrastive.py
@@ -0,0 +1,5 @@
+'''
+Implementation Non-Contrastive Learning 
+paper BYOL: https://arxiv.org/abs/2006.07733?fileGuid=WyYwxqq8kWjKdWgd
+Github Implementation: https://github.com/lucidrains/byol-pytorch
+'''
