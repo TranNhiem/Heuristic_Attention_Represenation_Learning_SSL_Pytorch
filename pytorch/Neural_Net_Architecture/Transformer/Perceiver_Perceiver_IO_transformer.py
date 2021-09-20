a4ee43f064eb4609f4e589731476f0615d72a073
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2021-09-20 11:30:36 +0800

    Create Perceiver_Perceiver_IO_transformer.py

---
+++ pytorch/Neural_Net_Architecture/Transformer/Perceiver_Perceiver_IO_transformer.py
@@ -0,0 +1,5 @@
+'''
+Implementation Perceiver and Perceiver_IO Cross Attention module for Computer Vision
+paper: https://arxiv.org/abs/2103.03206
+Github: https://github.com/lucidrains/perceiver-pytorch
+'''
