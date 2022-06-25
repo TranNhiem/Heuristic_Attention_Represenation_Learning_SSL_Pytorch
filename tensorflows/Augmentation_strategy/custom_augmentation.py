e369c5a772c0215154a3554eca70f941303aff69
TranNhiem <tvnhiemhmus@g.ncu.edu.tw>
2022-06-25 17:39:11 +0800

    configuration

--- tensorflows/Augmentation_strategy/custom_augmentation.py
+++
@@ -1,8 +0,0 @@
-
-'''
-This implementation will Develope in the future 
-## Define all your Augmenation Opts you can Deploy
-
-https://github.com/google-research/fixmatch/tree/master/imagenet/augment
-
-'''
